109404253a8b34f52404f3bb65b32916e8631fef
trianglestrip <user@example.com>
2026-09-14 11:13:48 +0800

    chore: remove temp files from merge

--- run-doc-sync.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set LOG=%TEMP%\dsh-doc-sync.log
-cd /d D:\gitProject\testCAD\portable\deepseek-harness
-echo START %DATE% %TIME% > "%LOG%"
-call pnpm run doc-sync >> "%LOG%" 2>&1
-echo EXIT=%ERRORLEVEL% >> "%LOG%"
